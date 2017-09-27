70f6e2bfa525a8476643e9db36e17d964143529b
Paul Fisher <paulfisher53@gmail.com>
2017-09-27 11:24:40 +1000

    chore(project): add project settings

---
+++ typings/JumpStart.d.ts
@@ -0,0 +1,29 @@
+declare interface CodeThread {
+}
+
+declare interface Controller {
+}
+
+declare interface Page {
+}
+
+declare interface PageField {
+}
+
+declare interface PageAction {
+}
+
+declare interface Table {
+}
+
+declare interface Column {
+}
+
+declare interface AppComponent {
+}
+
+declare interface HeaderComponent {
+}
+
+declare interface PageComponent {
+}
