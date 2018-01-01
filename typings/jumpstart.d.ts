6163446f1257b294790ec552e25f0c689d5359e5
Paul Fisher <paulfisher53@gmail.com>
2018-01-01 22:51:50 +1100

    style(typings): apply typescript best practices

--- typings/jumpstart.d.ts
+++ typings/jumpstart.d.ts
@@ -1,36 +1,43 @@
-interface JSApp {
-    confirm?(msg:string,callback:(ret:boolean) => void,title?:string,yescaption?:string,nocaption?:string):void;
-    ready?(callback:Function):void;
+declare namespace Jumpstart {
+
+    interface App {
+        confirm?(msg: string, callback: (ret: boolean) => void, title?: string, yescaption?: string, nocaption?: string): void;
+        ready?(callback: Function): void;
+    }
+
+    interface Event {
+        fire?(...args: any): void;
+    }
+
+    interface Log {
+        debugMode?(flag: boolean): void;
+        info?(msg: string): void;
+        error?(msg: string): void;
+        warning?(msg: string): void;
+        debug?(msg: string): void;
+        object?(msg: Object): void;
+    }
+
+    interface Util {
+        noThrow?(func: Function): void;
+    }
+
+    interface Window {
+        get?(): Window;
+        set?(win: (Window | Object)): void;
+    }
+
+    interface Scope {
+        app: App;
+        event: Event;
+        log: Log;
+        util: Util;
+        window: Window;
+    }
 }
 
-interface JSEvent {
-    fire?(...args:any):void;
-}
-
-interface JSLog {
-    debugMode?(flag:boolean):void;
-    info?(msg:string):void,
-    error?(msg:string):void,
-    warning?(msg:string):void,
-    debug?(msg:string):void,
-    object?(msg:Object):void
-}
-
-interface JSUtil {    
-    noThrow? (func:Function):void;
-}
-
-interface JSWindow {
-    get? ():Window;
-    set? (win:(Window|Object)):void;
-}
-
-interface JSScope {
-    app:JSApp,
-    event:JSEvent;
-    log:JSLog,
-    util:JSUtil;
-    window:JSWindow;
-}
-
-declare function jumpstart(fn:((scope:JSScope)=>void)):void;
+/**
+ * Jumpstart global function. Used externally to access Jumpstart modules, etc.
+ * @param fn - Callback function that will be passed the Jumpstart scope.
+ */
+declare function jumpstart(fn: ((scope: Jumpstart.Scope) => void)): void;
