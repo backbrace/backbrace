18b41424449e182ebf964077f320a4a2619d6bd7
Paul Fisher <paulfisher53@gmail.com>
2018-01-02 18:06:14 +1100

    docs($http): add overview and example

--- src/http.js
+++ src/http.js
@@ -1,11 +1,34 @@
 /**
- * HTTP module.
+ * Send various HTTP requests.
+ * @module $http
+ *
+ * @description
+ * The $http module is used to send various types of HTTP requests (like GET,POST,etc).
+ *
+ * The $http module is simply a wrapper for the
+ * [JQuery AJAX api](http://api.jquery.com/jquery.ajax). The JQuery AJAX calls are wrapped in a
+ * JQueryPromise so we can inject these functions directly into a $code.block.
+ *
+ * ## General use
+ * ```js
+ *  // Simple GET request with a $code.block:
+ *  return $code.block(
+ *      function(){
+ *          return $http.get('/someurl');
+ *      },
+ *      function(data){
+ *          // data from the GET request. NOTE: If the request
+ *          // failed, data will be `null`
+ *      }
+ *  );
+ * ```
  */
 'use strict';
 
 /**
  * Send a HTTP get request.
- * @param {string} url URL to get.
+ * @memberof module:$http
+ * @param {string} url Absolute or relative URL to get.
  * @returns {JQueryPromise} Promise to get the URL.
  */
 function get(url) {
