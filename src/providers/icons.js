b7be75b0fcd596ad99a5f6cc580282659e33e644
Paul Fisher <paulfisher53@gmail.com>
2018-01-04 15:31:21 +1100

    feat($icons): add icon provider

---
+++ src/providers/icons.js
@@ -0,0 +1,46 @@
+/**
+ * Icons provider module. By default, it gets icons from Material Design Icons Webfont
+ * (https://materialdesignicons.com/).
+ * @module $icons
+ */
+'use strict';
+
+/** @type {Jumpstart.IconProviderInstance} */
+var instance = {
+    get: function(name, size) {
+
+        // Set defaults.
+        name = name || 'alert';
+        size = size || 15;
+
+        // Prepend mdi- if missing.
+        if (name.indexOf('mdi-') !== 0)
+            name = 'mdi-' + name;
+
+        return '<i class="mdi ' + name + '" style="font-size: ' + size + 'px" />';
+    }
+};
+
+/**
+ * Get the icon provider instance.
+ * @memberof module:$icons
+ * @returns {Jumpstart.IconProviderInstance} Returns the icon provider instance.
+ */
+function get() {
+    return instance;
+}
+
+/**
+ * Set the icon provider instance.
+ * @memberof module:$icons
+ * @param {Jumpstart.IconProviderInstance} val Icon provider instance to set.
+ * @returns {void}
+ */
+function set(val) {
+    instance = val;
+}
+
+module.exports = {
+    get: get,
+    set: set
+};
