55228281e264ce10d75cd1cc408497efed1eb0fc
Paul Fisher <paulf@liveapp.com.au>
2018-04-12 10:28:04 +1000

    feat(meta): merge pages with a set of default values

--- src/meta.js
+++ src/meta.js
@@ -6,8 +6,40 @@
 'use strict';
 
 var code = require('./code'),
+    http = require('./http'),
     settings = require('./settings'),
-    http = require('./http');
+    util = require('./util'),
+    defaults = {
+        /** @type {PageMeta} */
+        page: {
+            name: '',
+            caption: '',
+            type: 'Card',
+            controller: '',
+            icon: '',
+            factbox: false,
+            actions: [],
+            tabs: []
+        },
+        /** @type {PageActionMeta} */
+        pageaction: {
+            name: '',
+            text: '',
+            icon: '',
+            classname: '',
+            desktopOnly: false,
+            mobileOnly: false
+        },
+        /** @type {PageTabMeta} */
+        pagetab: {
+            name: '',
+            text: '',
+            pageName: '',
+            desktopOnly: false,
+            mobileOnly: false,
+            factbox: false
+        }
+    };
 
 /**
  * Get page object meta data.
@@ -19,6 +51,32 @@ function page(name) {
         function() {
             // Get the page from a JSON file.
             return http.get(settings.meta.dir + 'pages/' + name + '.json');
+        },
+        function(json) {
+
+            // Merge the json with default values.
+            var $ = require('../external/jquery')();
+
+            // Extend the page.
+            /** @type {PageMeta} */
+            json.caption = json.caption || json.name;
+            var pge = $.extend({}, defaults.page, json);
+
+            // Extend the page actions.
+            pge.actions = [];
+            util.forEach(json.actions, function(action) {
+                action.text = action.text || action.name;
+                pge.actions.push($.extend({}, defaults.pageaction, action));
+            });
+
+            // Extend the page tabs.
+            pge.tabs = [];
+            util.forEach(json.tabs, function(tab) {
+                tab.text = tab.text || tab.name;
+                pge.tabs.push($.extend({}, defaults.pagetab, tab));
+            });
+
+            return pge;
         }
     );
 }
