93f97a1aa70bd6090c92eb4a61967550bf724d11
Paul Fisher <paulf@liveapp.com.au>
2018-08-14 15:54:07 +1000

    feat(format): add `format` module

---
+++ src/format.js
@@ -0,0 +1,25 @@
+/**
+ * @description
+ * Formatter module.
+ * @module format
+ */
+
+import { isDefined } from './util';
+import { get as moment } from './providers/moment';
+
+/**
+ * Format a field value to a string.
+ * @param {PageFieldMeta} field Page field.
+ * @param {*} value Value to format.
+ * @returns {string} Returns the formatted field value.
+ */
+export function formatField(field, value) {
+
+    if (value === null || !isDefined(value))
+        return value;
+
+    if (field.type === 'Date')
+        return moment(value).format('DD/MM/YYYY');
+
+    return value;
+}
