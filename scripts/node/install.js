30d73721132497fc112b7450cca156ee551473e5
Paul Fisher <paulfisher53@gmail.com>
2019-05-02 10:11:02 +1000

    build(npm): update dependencies

--- scripts/node/install.js
+++ scripts/node/install.js
@@ -4,6 +4,5 @@ function npm_install(where) {
     child_process.execSync('npm install', { cwd: where, env: process.env, stdio: 'inherit' })
 }
 
-npm_install('./packages/backbrace-packages');
 npm_install('./packages/backbrace-sample-app');
 npm_install('./packages/backbrace-docs');
