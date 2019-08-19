dcc374b19b0516c0c7e4d1f4ecbea11835ef44f0
Paul Fisher <paulfisher53@gmail.com>
2019-08-19 10:05:31 +1000

    fix(core): service worker not loading

--- packages/backbrace-docs/src/scripts/app.js
+++ packages/backbrace-docs/src/scripts/app.js
@@ -39,7 +39,7 @@
 
     // Register the service worker.
     if ('serviceWorker' in window.navigator)
-        window.navigator.serviceWorker.register((!backbrace.globals.DEVMODE ? './backbrace' : '') + '/service-worker.js' + (backbrace.settings().debug ? '?debug=true' : ''))
+        window.navigator.serviceWorker.register('/service-worker.js' + (backbrace.settings().debug ? '?debug=true' : ''))
             .then(function(reg) {
                 backbrace.serviceWorker(reg);
             });
