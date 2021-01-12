280f1ff165f3f04ab0bcd3c3d4e46384b8913fa7
Paul Fisher <paulfisher53@gmail.com>
2021-01-13 09:26:49 +1100

    ✨  Added new example app `auth`

---
+++ packages/backbrace-docs/src/examples/auth/design/app.js
@@ -0,0 +1,9 @@
+import {
+    start,
+    publicPath
+} from '../../../node_modules/@backbrace/core/dist/Backbrace.js';
+
+publicPath('../../../node_modules/@backbrace/core/dist/');
+
+// Start the app!
+start();
