cdd2866164c42ec36d3f1480eb5225c2358c7903
Paul Fisher <paulfisher53@gmail.com>
2021-02-02 15:17:40 +1100

    🔥  Remove docs examples

--- packages/backbrace-docs/src/examples/auth/design/app.js
+++
@@ -1,9 +0,0 @@
-import {
-    start,
-    publicPath
-} from '../../../node_modules/@backbrace/core/dist/Backbrace.js';
-
-publicPath('../../../node_modules/@backbrace/core/dist/');
-
-// Start the app!
-start();
